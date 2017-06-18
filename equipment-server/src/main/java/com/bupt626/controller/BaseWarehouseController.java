4c9f58b1dca2ab890fdb8997152fa4d30c98e246
mengying1204 <985216509@qq.com>
2017-06-18 20:38:59 +0800

    添加BaseWarehouse

--- equipment-server/src/main/java/com/bupt626/controller/BaseWarehouseController.java
+++ equipment-server/src/main/java/com/bupt626/controller/BaseWarehouseController.java
@@ -8,6 +8,7 @@ import com.bupt626.service.BaseWarehouseService;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -59,5 +60,10 @@ public class BaseWarehouseController extends BaseCommonController {
         }else {
             return sendFailMessage();
         }
+    }
+
+    @RequestMapping("/demo")
+    public String demoPage(){
+        return "demo";
     }
 }
