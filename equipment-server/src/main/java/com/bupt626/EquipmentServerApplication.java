1eef8da1d33be150885b61df53fbd114ec7f31fb
xiuyuhe <xyhe@qingyuanos.com>
2017-06-24 17:15:39 +0800

    后台 认证

--- equipment-server/src/main/java/com/bupt626/EquipmentServerApplication.java
+++ equipment-server/src/main/java/com/bupt626/EquipmentServerApplication.java
@@ -11,18 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @SpringBootApplication
 //@EnableDiscoveryClient
-public class EquipmentServerApplication extends WebSecurityConfigurerAdapter {
-
-	/**
-	 * Option method will send before get or other methods, cause it will check if it support csrf. It was
-	 * sent by browser, So will should let Option method go without authentication
-	 * @param http
-	 * @throws Exception
-	 */
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.requestMatchers().antMatchers(HttpMethod.OPTIONS, "**");
-	}
+public class EquipmentServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EquipmentServerApplication.class, args);
