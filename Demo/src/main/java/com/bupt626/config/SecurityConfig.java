ae6fdd236f4bf707e03d9108cc5323754ee6e629
xiuyuhe <xyhe@qingyuanos.com>
2017-06-25 15:27:06 +0800

    Demo  service  使用Feigin Client 访问 Auth ，配置Feign 的Intercept

--- Demo/src/main/java/com/bupt626/config/SecurityConfig.java
+++ Demo/src/main/java/com/bupt626/config/SecurityConfig.java
@@ -3,15 +3,16 @@ package com.bupt626.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  * Created by hexiuyu on 2017/1/18.
  */
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//@Configuration
-//@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+@EnableResourceServer
 public class SecurityConfig extends ResourceServerConfigurerAdapter{
     @Override
     public void configure(HttpSecurity http) throws Exception {
