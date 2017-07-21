fadf11dfe59affeefd81fae76edb05fef4e262f1
ycliu626 <1012351366@qq.com>
2017-07-21 19:58:01 +0800

    Merge branch 'develop' of https://github.com/xiuyuhe/SAM
    Conflicts:
    	equipment-server/README.md
    	equipment-server/src/main/java/com/bupt626/repository/AssetRepository.java

--- shop/src/main/java/com/bupt/ShopApplication.java
+++ shop/src/main/java/com/bupt/ShopApplication.java
@@ -3,16 +3,15 @@ package com.bupt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 //@EnableDiscoveryClient
 @EnableFeignClients
-public class EbookApplication {
+public class ShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EbookApplication.class, args);
+		SpringApplication.run(ShopApplication.class, args);
 	}
 }
