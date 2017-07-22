825416aff2e5caf80d46f8d6fb870a81b309089a
xiuyuhe <xyhe@qingyuanos.com>
2017-07-22 23:05:44 +0800

    delete shop

--- equipment-server/src/main/java/com/bupt626/api/EquipmentController.java
+++ equipment-server/src/main/java/com/bupt626/api/EquipmentController.java
@@ -2,11 +2,15 @@ package com.bupt626.api;
 
 import com.bupt626.domain.Equipment;
 import com.bupt626.service.EquipmentServiceImpl;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -40,5 +44,44 @@ public class EquipmentController {
     @RequestMapping("/getUserInfo")
     public String getUserInfo(){
         return "棒极了！哈哈";
+    }
+
+    @PostMapping("/testImage")
+    public void testFile(@RequestBody String  imageValue) {
+        String imageStr = imageValue.split(",")[1];
+        byte[] imageByte= Base64.decodeBase64(imageStr);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("xxasdf.png");
+            fileOutputStream.write(imageByte);
+            fileOutputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    static class Try{
+        String name;
+        String age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAge() {
+            return age;
+        }
+
+        public void setAge(String age) {
+            this.age = age;
+        }
     }
 }
--- ui/src/app/dashboard/dashboard.component.ts
+++ ui/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { DemoService } from '../common/service/demo.service';
-import {HttpInterceptor} from "../common/auth/HttpInterceptor";
-import { Observable } from 'rxjs';
+import {Component, OnInit, ViewChild} from "@angular/core";
+import {DemoService} from "../common/service/demo.service";
 import {UploadService} from "../common/service/upload.service";
-import {Http, Request, RequestOptionsArgs, Response, RequestOptions, ConnectionBackend, Headers} from "@angular/http";
+import {Http} from "@angular/http";
 import {LocalStorageService} from "../common/local-storage.service";
+import {FileUpload} from "primeng/components/fileupload/fileupload";
+import {HttpInterceptor} from "../common/auth/HttpInterceptor";
 
-
+declare let $;
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -14,38 +14,62 @@ import {LocalStorageService} from "../common/local-storage.service";
 })
 export class DashboardComponent implements OnInit {
   uploadedFiles: any[] = [];
-  url:string = "http://10.101.166.144:8755/equipments/upload";
-  constructor(private _localStorageService: LocalStorageService,private http: HttpInterceptor,private demoService: DemoService,private uploadService:UploadService) { }
-
-  ngOnInit(): void {
-    this.getDemos();
-  }
-
+  url:string = "http://10.101.166.144:8755/equipments/testImage";
   data: any;
-  getDemos() {
-    this.demoService.getDemo().subscribe(
-      (response) => {
-        this.data=response.json();
+  name: string;
+  image:any;
 
-       // this.data = JSON.stringify(response.json());
-        //console.log(this.data);
-       // console.log(this.data.id)
-        // this.data = response
-      }
-    );
+  @ViewChild("myProfile") fileUploadModule:FileUpload;
+
+  constructor(private _localStorageService: LocalStorageService,private http: HttpInterceptor,private demoService: DemoService,private uploadService:UploadService) { }
+  ngOnInit(): void {
+    this.fileUploadModule.showUploadButton = false;
   }
 
-  private onBeforeSend(event) {
-    console.log("before")
-    event.xhr.setRequestHeader("Authorization", "Bearer " + this._localStorageService.getAuth('access_token'));
+  onSubmit(){
+    // let options = new RequestOptions();
+    // options.method = RequestMethod.Post;
+    // options.headers = new Headers();
+    // options.headers.append('Authorization', 'Bearer ' + this._localStorageService.getAuth('access_token'));
+    // options.headers.append("Content-Type", "")
+    // let formParams = new FormData();
+    // console.log(options)
+    // formParams.append('file',this.fileUploadModule.files[0]);
+    // options.body = formParams;
+    // console.log(options);
+    // this.http.request(this.url, options).subscribe(()=>{});
+    //
+    //
+    // var xhr = new XMLHttpRequest();
+    // xhr.setRequestHeader("")
+
+
+    // let formParams = new FormData();
+    // let data = "name=hexiuyu&age=10"
+    // // formParams.append("data", JSON.stringify(data));
+    // formParams.append("data", data);
+    // formParams.append('file',this.fileUploadModule.files[0]);
+    // $.ajax({
+    //   url:this.url,
+    //   type: 'POST',
+    //   data: formParams,
+    //   processData: false,
+    //   contentType: false,
+    //   headers:{
+    //     "Authorization":'Bearer '+ this._localStorageService.getAuth('access_token')
+    //   }
+    // });
+
+
+
+    var myReader:FileReader = new FileReader();
+    myReader.onloadend = (e) => {
+      this.image = myReader.result;
+      console.log(this.image);
+      this.http.post(this.url,this.image).subscribe(()=>{})
+    };
+    myReader.readAsDataURL(this.fileUploadModule.files[0]);
+
   }
 
-  private onBeforeUpload(event){
-    console.log("add")
-    event.formData.append("name","hxy");
-  }
-
-  onUpload(event) {
-    console.log("onUpload")
-  }
 }
